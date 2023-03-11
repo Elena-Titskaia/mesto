c96797c2f7fbd2eefc771ccc8ec91baa35194bdf
Elena-Titskaia <120339250+Elena-Titskaia@users.noreply.github.com>
2023-03-11 15:56:12 +0300

    review version 2

--- script/index.js
+++ script/index.js
@@ -27,33 +27,6 @@ const popupCloseFullImageView = popupFullView.querySelector('.popup__close_type_
 const popupImageCard = popupFullView.querySelector('.popup__image-card');
 const popupImageName = popupFullView.querySelector('.popup__image-name');
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-]; 
-
 initialCards.forEach(object => {
   elementsArea.append(createElement(object.name, object.link))
 });
